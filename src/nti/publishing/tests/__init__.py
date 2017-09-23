26b0d700e7141c36a5c510fed26cc7fbdd6fe3ce
Carlos Sanchez <papachoco@gmail.com>
2017-09-22 19:40:49 -0500

    pylint

--- src/nti/publishing/tests/__init__.py
+++ src/nti/publishing/tests/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, absolute_import, division
-__docformat__ = "restructuredtext en"
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
@@ -33,7 +34,7 @@ class SharedConfiguringTestLayer(ZopeComponentLayer,
         zope.testing.cleanup.cleanUp()
 
     @classmethod
-    def testSetUp(cls, test=None):
+    def testSetUp(cls, unused_test=None):
         setHooks()
 
     @classmethod
